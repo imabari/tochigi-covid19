016b9a716ede3dd12e8c43a2ca4b1418188d4421
imabari <32343845+imabari@users.noreply.github.com>
2020-09-07 13:25:47 +0900

    skipfooter=1に戻し、データが４個以上ない行は削除

--- main.py
+++ main.py
@@ -82,7 +82,8 @@ tag_kanja = soup.find("a", text=re.compile("^栃木県における新型コロ�
 
 link_kanja = urljoin(url, tag_kanja.get("href"))
 
-df_kanja = pd.read_excel(link_kanja, header=1, skipfooter=2)
+df_kanja = pd.read_excel(link_kanja, header=1, skipfooter=1)
+df_kanja.dropna(thresh=5, inplace=True)
 
 df_kanja.loc[:, ["番号", "年代", "性別", "居住地"]] = df_kanja.loc[
     :, ["番号", "年代", "性別", "居住地"]
